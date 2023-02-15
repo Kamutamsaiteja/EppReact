546ab66d826865804a647f02cd2409b4c0623d08
9327105 <9327105@margadarsicomputers.com>
2023-02-15 17:24:09 +0530

    Updates

---
+++ precache-manifest.78ba30bab7d8b75d1233c80bf1da93c6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ac8dbd51a64a545a92d",
+    "url": "/EppReact/static/css/main.9ec2b182.chunk.css"
+  },
+  {
+    "revision": "9ac8dbd51a64a545a92d",
+    "url": "/EppReact/static/js/main.9ac8dbd5.chunk.js"
+  },
+  {
+    "revision": "e578d62e99c37642c8c0",
+    "url": "/EppReact/static/css/1.86b2b950.chunk.css"
+  },
+  {
+    "revision": "e578d62e99c37642c8c0",
+    "url": "/EppReact/static/js/1.e578d62e.chunk.js"
+  },
+  {
+    "revision": "17bea8dfb388d91836d8",
+    "url": "/EppReact/static/js/runtime~main.17bea8df.js"
+  },
+  {
+    "revision": "1191fa81a5e5a3463c36b17d9cba47c8",
+    "url": "/EppReact/static/media/logo.1191fa81.png"
+  },
+  {
+    "revision": "9ac10ec536d6683e0a9a517fc32e2c46",
+    "url": "/EppReact/static/media/mb1.9ac10ec5.jpg"
+  },
+  {
+    "revision": "ef061fd0c9206fa7d125211e95dd40cc",
+    "url": "/EppReact/static/media/mb2.ef061fd0.jpg"
+  },
+  {
+    "revision": "7b0a1589b5286632d0687be5e4614966",
+    "url": "/EppReact/static/media/mb3.7b0a1589.jpg"
+  },
+  {
+    "revision": "6c3912ffa5e93ddcb6aac6a70f8b1937",
+    "url": "/EppReact/static/media/development.6c3912ff.jpg"
+  },
+  {
+    "revision": "63212bf6b8bf60e1e7a1067860f1f033",
+    "url": "/EppReact/index.html"
+  }
+];
